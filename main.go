6781a1c11449906da2553602c5ff5c86582d0096
Dewald Visser <dewald.visser@gmail.com>
2020-10-13 21:50:19 +0300

    change message once again

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	log.Println("Starting helloworld application...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello cruel universe again!\n")
+		fmt.Fprintf(w, "Hello cruel universe once again!\n")
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
